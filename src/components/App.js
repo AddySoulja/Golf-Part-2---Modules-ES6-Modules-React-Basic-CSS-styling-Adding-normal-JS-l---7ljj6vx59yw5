2cd04f90c88b5354f4e946ee0c2658f3ac9a8b2b
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-01-21 14:35:34 +0000

    modded

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,59 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
-  });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  useEffect(() => {
+    const handler = (e) => {
+      if (e.key === "ArrowRight") {
+        setX(x + 5);
+      }
+      if (e.key === "ArrowUp") {
+        setY(y - 5);
+      }
+      if (e.key === "ArrowLeft") {
+        setX(x - 5);
+      }
+      if (e.key === "ArrowDown") {
+        setY(y + 5);
+      }
+    };
+
+    window.addEventListener("keydown", handler);
+
+    return () => {
+      window.removeEventListener("keydown", handler);
+    };
+  }, [x, y]);
+
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+  };
+
+  const renderChoice = () => {
+    if (renderBall) {
+      return (
+        <div
+          className="ball"
+          style={{
+            left: `${x}px`,
+            top: `${y}px`
+          }}
+        ></div>
+      )
+    }
+    return (
+      <button
+        onClick={() => setRenderBall(true)}
+        className="start"
+      >Start</button>
+    );
+  };
 
   return (
     <div className="playground">
